955ef2d48957a2d21a1a278d0ab25f1167958379
ljonathanl <jonathan.gourdin@gmail.com>
2021-03-24 16:23:51 +0100

    Add persistence

--- index.js
+++ index.js
@@ -1,45 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const storage = require('node-persist');
 
 const app = express()
 app.use(express.json())
 app.use(express.static('public'))
 app.use(morgan('tiny'))
 
-var accounts = {
-    alice: {name: 'alice', balance: 0, creator: true, date: new Date().toISOString()},
-    bob: {name: 'bob', balance: 0, creator: true, date: new Date().toISOString()},
-    claude: {name: 'claude', balance: 0, creator: true, date: new Date().toISOString()},
-    daniel: {name: 'daniel', balance: 0, creator: false, date: new Date().toISOString()},
-}
-
-var ideas = {
-    idea1: {name: 'idea1', account: 'alice', text: 'first idea', date: new Date().toISOString()},
-}
-
-var currency = {
-    date: new Date().toISOString(),
-    c: 0.1,
-    stepTime: 60,
-    elapsedTime: 0,
-    revaluationTime: 30, 
-    playing: true, 
-}
-
-var transactions = [];
-
-
-function play() {
-    if (currency.playing) {
-        currency.elapsedTime++;
-        Object.values(accounts).forEach( account => {
-            if (currency.elapsedTime % currency.revaluationTime == 0)
-                account.balance = Math.ceil(account.balance * (1 - currency.c));
-            account.balance += 100;
-        });
-    }
-    setTimeout(play, currency.stepTime * 1000);
-}
+let currency = {}
+let accounts = {}
+let transactions = []
+let ideas = {}
 
 /* 
  * CURRENCY
@@ -63,28 +34,33 @@ app.get('/accounts', (req, res) => {
     res.status(200).json(Object.values(accounts))
 })
 
-app.post('/accounts', (req, res) => {
+app.post('/accounts', async (req, res) => {
     var account = accounts[req.body.name] = Object.assign({name: '', balance: 0, creator: false, date: new Date().toISOString()}, req.body)
+    await storage.setItem('accounts', accounts)
     res.status(201).json(account)
 })
 
-app.patch('/accounts/:id', (req, res) => {
+app.patch('/accounts/:id', async (req, res) => {
     const id = req.params.id
     var account = accounts[id]
-    if (account)
+    if (account) {
         account = Object.assign(account, req.body)
+        await storage.setItem('accounts', accounts)    
+    }
     res.status(account ? 200 : 404).json(account)
 })
 
-app.delete('/accounts/:id', (req, res) => {
+app.delete('/accounts/:id', async (req, res) => {
     const id = req.params.id
     var account = accounts[id]
-    if (account)
+    if (account) {
         delete accounts[id]
+        await storage.setItem('accounts', accounts)
+    }
     res.status(account ? 204 : 404).send();
 })
 
-app.post('/tx', (req, res) => {
+app.post('/tx', async (req, res) => {
     var tx = Object.assign({from: '', to: '', value: 0, date: new Date().toISOString()}, req.body)
     var accountFrom = accounts[tx.from]
     var accountTo = accounts[tx.to]
@@ -95,6 +71,8 @@ app.post('/tx', (req, res) => {
         accountFrom.balance -= tx.value
         transactions.push(tx)
         res.status(201).json(tx)
+        await storage.setItem('accounts', accounts)
+        await storage.setItem('transactions', transactions)
     }
 })
 
@@ -140,4 +118,62 @@ app.listen(80, () => {
     console.log("Let the Füne begin !!!")
 })
 
-play()
+
+async function start() {
+    await storage.init({dir: 'db'})
+
+    currency = await storage.getItem('currency')
+
+    if (!currency)
+        currency = {
+            date: new Date().toISOString(),
+            c: 0.1,
+            stepTime: 60,
+            elapsedTime: 0,
+            revaluationTime: 30, 
+            playing: true, 
+        }
+
+
+    accounts = await storage.getItem('accounts')
+
+    if (!accounts)
+        accounts = {
+            alice: {name: 'alice', balance: 0, creator: true, date: new Date().toISOString()},
+            bob: {name: 'bob', balance: 0, creator: true, date: new Date().toISOString()},
+            claude: {name: 'claude', balance: 0, creator: true, date: new Date().toISOString()},
+            daniel: {name: 'daniel', balance: 0, creator: false, date: new Date().toISOString()},
+        }
+
+    ideas = {
+        idea1: {name: 'idea1', account: 'alice', text: 'first idea', date: new Date().toISOString()},
+    }
+
+
+
+    transactions = await storage.getItem('transactions')
+
+    if (!transactions)
+        transactions = [];
+
+    play()
+}
+
+
+
+async function play() {
+    if (currency.playing) {
+        currency.elapsedTime++;
+        Object.values(accounts).forEach( account => {
+            if (currency.elapsedTime % currency.revaluationTime == 0)
+                account.balance = Math.ceil(account.balance * (1 - currency.c));
+            account.balance += 100;
+        });
+        await storage.setItem('accounts', accounts)
+        await storage.setItem('currency', currency)
+    }
+    setTimeout(play, currency.stepTime * 1000);
+}
+
+
+start();
