d1bdcb6b8f845a48b36a5997f5cf2b5a9551b182
BCOM\jgourdin <jonathan.gourdin@b-com.com>
2021-05-08 16:45:13 +0200

    Vue component

---
+++ client/Fune/src/components/Funiter-Currency.vue
@@ -0,0 +1,151 @@
+<template>
+    <div>
+        <h4 class="text-center">
+            <en>currency</en>
+            <fr>monnaie</fr>
+        </h4>
+        <form>
+            <div class="input-group mb-3">
+                <label class="input-group-text w-50">
+                    <en>Ü revaluation</en>
+                    <fr>réévaluation de Ü</fr>
+                </label>
+                <select v-model="editedCurrency.mode" id="currencyMode" class="form-select w-50"
+                    :class="{'bg-primary': editedCurrency.mode != funiter.currency.mode}">
+                    <option value="melt">
+                        <en>melting</en>
+                        <fr>fonte</fr>
+                    </option>
+                    <option value="grew">
+                        <en>growth</en>
+                        <fr>croissance</fr>
+                    </option>
+                </select>
+            </div>   
+            <div class="input-group mb-3">
+                <span class="input-group-text" v-if="editedCurrency.mode == 'melt'">Ü(t-1) = ( 1 - </span>
+                <span class="input-group-text" v-else>Ü(t+1) = ( 1 + </span>
+                <input id="currencyExpression" v-model="editedCurrency.expression" type="text" class="form-control text-right"
+                    :class="{'bg-primary': editedCurrency.expression != funiter.currency.expression}">
+                <span class="input-group-text"> ) Ü(t)</span>    
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="currencyUPerDay">
+                    <en>Ü per day</en>
+                    <fr>Ü par jour</fr>
+                </label>
+                <input id="currencyUPerDay" v-model="editedCurrency.uPerDay" type="number" min="1" max="1000" step="1" class="form-control text-right"
+                    :class="{'bg-primary': editedCurrency.uPerDay != funiter.currency.uPerDay}">
+                <span class="input-group-text">Ü</span>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="currencyRevaluationTime">
+                    <en>revaluation period</en>
+                    <fr>réévaluation</fr>
+                </label>
+                <input id="currencyRevaluationTime" v-model="editedCurrency.revaluationTime" type="number" min="1" max="400" step="1" class="form-control text-right" 
+                    :class="{'bg-primary': editedCurrency.revaluationTime != funiter.currency.revaluationTime}">
+                <span class="input-group-text">
+                    <en>day<span v-if="editedCurrency.revaluationTime > 1">s</span></en>
+                    <fr>jour<span v-if="editedCurrency.revaluationTime > 1">s</span></fr>
+                </span>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="currencyStepTime">
+                    <en>day duration</en>
+                    <fr>durée d'un jour</fr>
+                </label>
+                <input id="currencyStepTime" v-model="editedCurrency.stepTime" type="number" min="1" max="90000" step="1" class="form-control text-right"
+                    :class="{'bg-primary': editedCurrency.stepTime != funiter.currency.stepTime}">
+                <span class="input-group-text">
+                    <en>second<span v-if="editedCurrency.stepTime > 1">s</span></en>
+                    <fr>seconde<span v-if="editedCurrency.stepTime > 1">s</span></fr>
+                </span>
+            </div>
+            <div class="d-grid gap-2 d-md-flex mb-3" v-if="currencyChange">
+                <button type="button" class="btn btn-secondary mr-auto" @click="Object.assign(editedCurrency, funiter.currency)">
+                    <en>ündo!</en>
+                    <fr>annüler!</fr>
+                </button>
+                <button type="button" class="btn btn-primary" @click="funiter.updateCurrency(editedCurrency)">
+                    <en>üpdate!</en>
+                    <fr>valïder!</fr>
+                </button>
+            </div>
+            <div class="input-group mb-3 mt-5">
+                <label class="input-group-text" for="currencyMass">
+                    <en>monetary mass (M)</en>
+                    <fr>masse monétaire (M)</fr>
+                </label>
+                <input id="currencyMass" :value="funiter.currencyWithoutDecimal(funiter.currency.monetaryMass)" type="text" class="form-control text-right" disabled>
+                <span class="input-group-text">Ü</span>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="currencyNbMembers">
+                    <en>Üman accounts (N)</en>
+                    <fr>comptes Ümains (N)</fr>
+                </label>
+                <input id="currencyNbMembers" :value="funiter.currency.nbMembers" type="text" class="form-control text-right" disabled>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="currencyAvg">
+                    <en>average</en>
+                    <fr>moyenne</fr>
+                </label>
+                <input id="currencyAvg" :value="funiter.currency.nbMembers > 0 ? funiter.currencyDecimal(funiter.currency.monetaryMass / funiter.currency.nbMembers) : 0" type="text" class="form-control text-right" disabled>
+                <span class="input-group-text">
+                    <en>Ü / Üman</en>
+                    <fr>Ü / Ümain</fr>
+                </span>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="currencyLastMelt">
+                    <en>last melting</en>
+                    <fr>dernière fonte</fr>
+                </label>
+                <input id="currencyLastMelt" :value="funiter.currency.lastMelt" type="text" class="form-control text-right" disabled>
+                <span class="input-group-text">%</span>
+            </div>
+            <div class="input-group mb-3">
+                <label class="input-group-text" for="currencyElapsedTime">
+                    <en>elapsed time (T)</en>
+                    <fr>temps écoulé (T)</fr>
+                </label>
+                <input id="currencyElapsedTime" v-model="funiter.currency.elapsedTime" type="text" class="form-control text-right" disabled>
+                <span class="input-group-text">
+                    <en>day<span v-if="funiter.currency.elapsedTime > 1">s</span></en>
+                    <fr>jour<span v-if="funiter.currency.elapsedTime > 1">s</span></fr>
+                </span>
+            </div>
+        </form>    
+    </div>
+</template>
+
+<script setup>
+import funiter from '../lib/funiterStandalone.js'
+import { fr, en } from './Translate.js'
+import { defineProps, reactive, computed, onMounted } from 'vue'
+
+const editedCurrency = reactive({
+    expression: '',
+    mode: 'melt',
+    uPerDay: 1,
+    stepTime: 0,
+    revaluationTime: 0,
+})
+
+const currencyChange = computed(() => {
+    if (!funiter || !funiter.currency)
+        return false
+    return funiter.currency.expression != editedCurrency.expression
+        || funiter.currency.mode != editedCurrency.mode
+        || funiter.currency.uPerDay != editedCurrency.uPerDay
+        || funiter.currency.revaluationTime != editedCurrency.revaluationTime
+        || funiter.currency.stepTime != editedCurrency.stepTime;
+})
+
+onMounted(() => {
+    Object.assign(editedCurrency, funiter.currency)
+})
+
+</script>
---
+++ client/Fune/src/components/Funiter-Stats.vue
@@ -0,0 +1,113 @@
+<template>
+    <div>
+        <h4 class="text-center">
+            <en>stats</en>
+            <fr>stats</fr> 
+        </h4>
+        <trends
+            :data="state.stats"
+            :colors="colors"
+            :padding="10"
+            :radius="8"
+            :stroke-width="1"
+            :stroke-linecap="'butt'"
+            :min="0"
+            auto-draw
+            smooth>
+        </trends>
+        <div class="w-100">
+            <div class="d-inline-block ml-4">
+                100
+                <select v-model="state.statType">
+                    <option value="day">
+                        <en>days</en>
+                        <fr>jours</fr>
+                    </option>
+                    <option value="month">
+                        <en>months</en>
+                        <fr>mois</fr>
+                    </option>
+                    <option value="year">
+                        <en>years</en>
+                        <fr>années</fr>
+                    </option>
+                </select> 
+            </div>
+            <div class="custom-control custom-switch d-inline-block mr-2" style="float: right;">
+                <input type="checkbox" class="custom-control-input" id="statsComparedToAverage" v-model="state.comparedToAverage">
+                <label class="custom-control-label text-left" for="statsComparedToAverage" style="width: 50px; vertical-align: baseline;">
+                    {{ state.comparedToAverage ? '%M/N' : 'Ü' }}
+                </label>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+import { Trends } from './Trend.js'
+import { fr, en } from './Translate.js'
+import funiter from '../lib/funiterStandalone.js'
+import { defineProps, watch, onMounted, reactive } from 'vue'
+
+const { colors } = defineProps({
+    colors: Array,
+})
+
+const state = reactive({
+    comparedToAverage: false,
+    statType: 'day',
+    stats: [],
+})
+
+watch(() => funiter.selectedAccounts,
+    () => {
+        getStats()
+    },
+    { deep: true }
+)
+
+watch(() => funiter.currency.elapsedTime,
+    () => {
+        getStats()
+    }
+)
+
+watch(() => state.comparedToAverage, 
+    () => {
+        getStats()
+    }
+)
+
+watch(() => state.statType, 
+    () => {
+        getStats()
+    }
+)
+
+const getStats = () => {
+    let funeStat = funiter.getAccountStats(funiter.name, state.statType)
+    for (let index = 1; index < funiter.selectedAccounts.length; index++) {
+        const account = funiter.selectedAccounts[index];
+        if (account) {
+            let stat = funiter.getAccountStats(account, state.statType)
+            if (state.comparedToAverage) {
+                for (let j = 0; j < stat.length; j++) {
+                    stat[j] = funeStat[j] == 0 ? 0 : (stat[j] / funeStat[j]) * 100000;
+                }
+            }
+            state.stats[index] = stat
+        }
+    }
+    if (state.comparedToAverage) {
+        funeStat = new Array(funeStat.length).fill(100000)
+    } 
+    state.stats[0] = funeStat
+}
+
+onMounted(() => {
+    getStats()
+})
+
+
+</script>
