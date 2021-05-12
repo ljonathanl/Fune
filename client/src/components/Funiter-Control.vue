2a96cc7d68e93064b7a28d5edd7814d4b0f356ba
BCOM\jgourdin <jonathan.gourdin@b-com.com>
2021-05-12 15:33:08 +0200

    translate control buttons

--- client/src/components/Funiter-Control.vue
+++ client/src/components/Funiter-Control.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="text-center"> 
-    <button class="btn btn-primary" @click="funiter.play()" :disabled="funiter.isPlaying"><i class="fa fa-play"></i> play</button>
-    <button class="btn btn-secondary" v-longclick="stepForward" :disabled="funiter.isPlaying"><i class="fa fa-step-forward"></i> next</button>
-    <button class="btn btn-secondary mr-5" @click="funiter.stop()" :disabled="!funiter.isPlaying"><i class="fa fa-pause"></i> pause</button>
+    <button class="btn btn-primary" @click="funiter.play()" :disabled="funiter.isPlaying">
+      <i class="fa fa-play"></i>
+      <en> play</en>
+      <fr> lecture</fr>
+    </button>
+    <button class="btn btn-secondary" v-longclick="stepForward" :disabled="funiter.isPlaying">
+      <i class="fa fa-step-forward"></i>
+      <en> next</en>
+      <fr> suivant</fr>
+    </button>
+    <button class="btn btn-secondary mr-5" @click="funiter.stop()" :disabled="!funiter.isPlaying">
+      <i class="fa fa-pause"></i>
+      <en> pause</en>
+      <fr> pause</fr>
+    </button>
 
 
-    <label class="btn btn-dark mb-0">
-      <i class="fa fa-upload"></i> load<input type="file" hidden @change="readSingleFile">
+    <label class="btn btn-dark mb-0" :class="{ disabled: funiter.isPlaying }">
+      <i class="fa fa-upload"></i>
+      <en> load</en>
+      <fr> charger</fr>
+      <input type="file" hidden @change="readSingleFile">
     </label>
-    <button class="btn btn-light text-dark" @click="save()"><i class="fa fa-download"></i> save</button>
+    <button class="btn btn-light text-dark" @click="save()" :disabled="funiter.isPlaying">
+      <i class="fa fa-download"></i>
+      <en> save</en>
+      <fr> sauvegarder</fr>
+    </button>
 
-    <button class="btn btn-danger mx-5" @click="funiter.reset()"><i class="fa fa-undo"></i> reset</button>
+    <button class="btn btn-danger mx-5" @click="funiter.reset()">
+      <i class="fa fa-undo"></i>
+      <en> reset</en>
+      <fr> RAZ</fr>
+    </button>
   </div>
 </template>
 
@@ -18,6 +41,7 @@
 import funiter from '../lib/funiterReactive.js'
 import jsonc from '../lib/JSONC.js'
 import { saveAs } from 'file-saver'
+import { fr, en, translate } from './Translate.js'
 
 
 const stepForward = () => {
