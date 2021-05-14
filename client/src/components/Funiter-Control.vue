694c5032f94680ed9a5106dbf3f8edc51d1943af
BCOM\jgourdin <jonathan.gourdin@b-com.com>
2021-05-14 05:17:39 +0200

    toggle play pause

--- client/src/components/Funiter-Control.vue
+++ client/src/components/Funiter-Control.vue
@@ -1,20 +1,21 @@
 <template>
   <nav class="navbar navbar-dark bg-dark">
     <div class="text-center w-100">
-      <button class="btn btn-primary" @click="funiter.play()" :disabled="funiter.isPlaying">
+      <span @click="toTop()" class="pointer bg-primary px-1 display-6 position-absolute" style="font-family: monospace;left: 0.5em;">fї</span>
+      <button class="btn btn-outline-primary mx-1" @click="funiter.play()" v-if="!funiter.isPlaying" style="width: 115px" >
         <i class="fa fa-play"></i>
         <en> play</en>
         <fr> lecture</fr>
       </button>
-      <button class="btn btn-outline-light mx-3" v-longclick="stepForward" :disabled="funiter.isPlaying">
-        <i class="fa fa-step-forward"></i>
-        <en> next</en>
-        <fr> suivant</fr>
-      </button>
-      <button class="btn btn-outline-light" @click="funiter.stop()" :disabled="!funiter.isPlaying">
+      <button class="btn btn-outline-light mx-1" @click="funiter.stop()" v-if="funiter.isPlaying" style="width: 115px">
         <i class="fa fa-pause"></i>
         <en> pause</en>
         <fr> pause</fr>
+      </button>
+      <button class="btn btn-outline-light" v-longclick="stepForward" :disabled="funiter.isPlaying">
+        <i class="fa fa-step-forward"></i>
+        <en> next</en>
+        <fr> suivant</fr>
       </button>
     </div>
   </nav>
@@ -27,6 +28,10 @@ import { fr, en } from './Translate.js'
 
 const stepForward = () => {
   funiter.play(false)
+}
+
+const toTop = () => {
+  window.scroll(0,0)
 }
 
 </script>
